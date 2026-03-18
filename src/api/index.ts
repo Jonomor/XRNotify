a28ea6951664e53fe47d7619a3489482e5c1500d
Ali Morgan <alimorgan@Alis-MacBook-Air.local>
2026-03-18 17:36:16 -0400

    Clean up old xrnotify1 files, remove all Dockerfiles

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * XRNotify API Module Exports
- */
-
-export { webhookRoutes } from './webhookRouter.js';
-export { authRoutes, authenticateApiKey } from './authRouter.js';
-export { deliveryRoutes } from './deliveryRouter.js';
