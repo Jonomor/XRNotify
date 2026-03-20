3053d82f2cc1964be07a904fd45047b48ae00a0f
Ali Morgan <alimorgan@mac.mynetworksettings.com>
2026-03-19 21:25:06 -0400

    Add brand assets: logo, favicons, manifest

--- apps/platform/.next/standalone/apps/platform/.next/static/chunks/app/docs/page-f303f076ca81530a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[726,301,910,966,793,702,931,979],{4659:function(n,e,u){Promise.resolve().then(u.t.bind(u,2629,23))}},function(n){n.O(0,[629,912,814,744],function(){return n(n.s=4659)}),_N_E=n.O()}]);
